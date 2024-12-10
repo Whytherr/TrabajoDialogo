5376ce9b6cae58c4bf5bf02552d37c018a36dbdc
Whytherr <22.sis.mariodomingo@fundacionmontessori.com>
2024-12-10 15:45:26 +0100

    Add files via upload

---
+++ TrabajoDialogo/Dialogo/src/conversacion/Dialogo.java
@@ -0,0 +1,37 @@
+package conversacion;
+
+public class Dialogo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("VICENTE");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("VICENTE");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("VICENTE");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("VICENTE");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("VICENTE");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("VICENTE");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("VICENTE");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("VICENTE");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("VICENTE");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+	}
+
+}
