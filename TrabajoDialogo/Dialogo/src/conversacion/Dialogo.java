85ba9d43576ef89ea2729312223dedacfe729f2d
Whytherr <22.sis.mariodomingo@fundacionmontessori.com>
2024-12-10 16:15:02 +0100

    Add files via upload

---
+++ TrabajoDialogo/Dialogo/src/conversacion/Dialogo.java
@@ -0,0 +1,40 @@
+package conversacion;
+
+public class Dialogo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("AMADOR");
+		
+		System.out.println("VICENTE: (hablando consigo mismo) La última croqueta de mi Gregoria. "
+				+ "Siempre decía que las hacía con amor… y vaya si tenía razón"
+				+ "Ayyy Gregoria como te echo de menos.");
+		
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("VICENTE: (melancólico) Aquí estoy, despidiéndome de mi Gregoria. "
+				+ "Esta croqueta fue lo último que cocinó para mí.");
+		
+		System.out.println("AMADOR");
+		System.out.println("MAXI");
+		System.out.println("VICENTE: Vicente: (esbozando una sonrisa) ¿Cañas? "
+				+ "Eso suena a algo que Gregoria aprobaría.");
+		
+		System.out.println("MAXI");
+		System.out.println("VICENTE: (emocionado) Y por la amistad. "
+				+ "Con vosotros, hasta lo más triste se convierte en algo llevadero.");
+		
+		System.out.println("AMADOR");
+		
+		System.out.println("FIN");
+		
+	}
+
+}
