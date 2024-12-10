11dec5fae1e32eacd0aef0bd6c7df76e254b43a7
Whytherr <22.sis.mariodomingo@fundacionmontessori.com>
2024-12-10 16:13:29 +0100

    Add files via upload

---
+++ TrabajoDialogo/TrabajoDialogo/Personaje/src/vicente/Elhombrecojin.java
@@ -0,0 +1,18 @@
+package vicente;
+
+public class Elhombrecojin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Vicente es un hombre mayor de unos 67 años, lleva muchos años dedicando su vida"
+				+ "ver el futbol y subir a casa de su hijo a comer, el cual vive 3 pisos mas arriba que él."
+				+ "Vicente mas conocido como el hombre cojin vivía tranquilo, hasta que un dia cuando subio del bar"
+				+ "encontro a su mujer en el suelo muerta, a partir de ahi Vicente entraria en una depresion y esto"
+				+ "significaria que su deseo de suicidarse aumentaria significablemente. ");
+		
+		
+		
+	}
+
+}
